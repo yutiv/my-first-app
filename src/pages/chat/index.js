93878ef1c0df27188b367c6eb672f9d5def5efbf
yutiv <o0533180283@gmail.com>
2025-02-26 12:30:49 +0200

    complete mobile

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-export default function HelloChat() {
+const HelloChat = () => {
 
     return (<>
         <h1>Chat</h1>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-export default function HelloHome() {
+const HelloHome = () => {
     return (<>
         <h1>Home</h1>
     </>
     );
-};
+}
 export { HelloHome }
