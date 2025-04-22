9bfb126db8726b57d6fd22b19dc8a9c8f61ebb46
yutiv <o0533180283@gmail.com>
2025-04-22 11:03:45 +0300

    avatar

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,59 +1,100 @@
 import React from "react";
 import "./index.css";
-import { FormControl, TextField, IconButton, InputAdornment } from "@mui/material";
+import { FormControl, TextField, IconButton, Avatar } from "@mui/material";
 import { Send } from "@mui/icons-material"
 const HelloChat = () => {
-    // const [avatar, setAvatar] = React.useState('')
-    // const [message1, setMessage1] = React.useState([]);
-    // const [message2, setMessage2] = React.useState([]);
-    const [messageall, setMessageall] = React.useState([{}]);
+    // let lastMes;
+    const [messageall, setMessageall] = React.useState([
+        { key: 'message2', value: 'ddd' },
+        // { key: 'message1', value: 'jjj' },
+        // { key: 'message1', value: 'lll' },
+        // { key: 'message2', value: 'ddd' },
+        // { key: 'message1', value: 'hjj' },
+        // { key: 'message2', value: 'sss' },
+        // { key: 'message2', value: 'eee' },
+    ]);
     const [valInput, setValInput] = React.useState('');
     const handleChange = (event) => {
         setValInput(event.target.value);
     };
 
     const user = () => {
-        // const newAvatar = document.createElement('Avatar');
-        if (document.getElementById('1')) {
-            // message1.push(valInput)
-            messageall.push({ key: "message1", value: valInput })
-            //   message1.push(message2)
-            //   message1.pop(message2)
-            // if (avatar !== 'I') {
-            // setAvatar('I')
-            // newAvatar.id = 'avatar'
-            // newAvatar.innerHTML = avatar
-            // container.appendChild(newAvatar);
-            // newElement.id = 'one'
-            //                 if(avatar=='I'){
-            //             newAvatar.id='other'
-            // }
-            // }
-            // container.appendChild(newAvatar);
-            // newElement.id='other'
-        }
-        else {
-            // message2.push(valInput)
-            messageall.push({ key: "message2", value: valInput })
-            // newAvatar.id = 'avatar1'
-            // if (avatar !== 'y') {
-            // setAvatar('y')
-            // newAvatar.innerHTML = avatar
-            // container.appendChild(newAvatar);
-            // newElement.id = 'tow'
-        }
+        // if (document.getElementById('1')) {
+        setMessageall(prevArray => [...prevArray, { key: "message1", value: valInput }]);
         setValInput('')
+        // } else {
+        //     setMessageall(prevArray => [...prevArray, { key: "message2", value: valInput }]);
+        //     setValInput('')
         // }
-        // container.appendChild(newAvatar);
+        // }
+    }
+    let lastMes = messageall[messageall.length - 1].key;
+    {
+        console.log(lastMes, " lll");
     }
     return (<>
         <div id="container">
             <div className="messages">
-                {messageall.map(m => (m.key) == "message1" ? <p className="message1">{m.value}</p> : <p className="message2">{m.value}</p>)}
+                <div>
+                    {messageall.map(m => (m.key) === "message1" ?
+                        <div>
+                            {console.log(lastMes, (m.key))}
+                            {lastMes !== (m.key) ?
+                                <div>
+                                    {console.log(lastMes, (m.key))}
+                                    <Avatar>{m.key.slice(7, 8)}</Avatar>
+                                    <p className="message1"> {m.value}</p>
+                                </div>
+                                :
+                                <p className="message1"> {m.value}</p>}
+                        </div>
+                        :
+                        <div>
+                            <div>
+                                {lastMes !== (m.key) ?
+                                    <div>
+                                        <Avatar className="message2">{m.key.slice(7, 8)}</Avatar>
+                                        <p className="message2"> {m.value}</p>
+                                    </div>
+                                    :
+                                    <p className="message2"> {m.value}</p>
+                                }
+                            </div>
+                            {/* <div>{console.log(lastMes)} */}
+                            {/* </div> */}
+                        </div>
+
+                    )}
+                </div>
+
+
+
+                {/* <div>{messageall.map(m =>lastMes == (m.key)
+                    ?
+                    <div>
+                        <Avatar className={m.key}>{m.key.slice(7, 8)}</Avatar>
+                        {(m.key) == "message1" ?
+                            <p className="message1"> {m.value}</p>
+                            :
+                            <p className="message2">{m.value}</p>}
+                    </div>
+                    : (m.key) == "message1" ?
+                        <p className="message1"> {m.value}</p>
+                        :
+                        <p>
+                            <p className="message2">{m.value}</p>
+                            {/* {console.log(lastMes = m.key, "  lastMes = m.key") */}
+                {/* } */}
+                {/* </p> */}
+                {/* )}
+                // </div> */}
+                {/* } */}
+
+
             </div>
             <div id="textField" >
                 <FormControl id="formControl">
-                    <TextField label="Type a message" onChange={handleChange}
+                    <TextField label="Type a message" onChange={handleChange} value={valInput}
                         InputProps={{
                             endAdornment: (
                                 <IconButton onClick={() => user()}><Send id="send" /></IconButton>
@@ -63,7 +104,7 @@ const HelloChat = () => {
                     </ TextField>
                 </FormControl>
             </div>
-        </div>
+        </div >
     </>
     );
 };
